2c0acf5e8210485c08014806083f1ffaccc0dbac
Scott Cottam <scott@scottcottam.co.uk>
2020-03-30 02:59:14 +0100

    Responsive nav, JS Menu Generator and associated CSS
    Refined JS, missing the ability to display if an item is vegetarian or not and a subheading on food items.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(){
-  console.log('DOM fully loaded and parsed');
-
   let requestURL = './menu.json';
 
   let request = new XMLHttpRequest();
@@ -14,65 +12,120 @@ document.addEventListener("DOMContentLoaded", function(){
 
     const section = menu['menuSection'];
 
-    section.forEach(key => {
+    section.forEach(menuSection => {
 
       //Create Subheader Div
-      let headDiv = document.createElement("div");
-      headDiv.setAttribute("class", "subHead");
+      let subHeaderDiv = document.createElement("div");
+      subHeaderDiv.setAttribute("class", "subHeader");
 
-      //
-      let mainDiv = document.getElementById("mainDiv");
-      mainDiv.appendChild(headDiv);
+      //Attach subheader div to section div
+      let mainDiv = document.getElementById("menuDiv");
+      mainDiv.appendChild(subHeaderDiv);
 
-      //Create Subheader H2
-      let subHeader = document.createElement("h2");
-      let node = document.createTextNode(key.name);
+      //Create Subheader H3
+      let subHeader = document.createElement("h3");
+      let node = document.createTextNode(menuSection.name);
       subHeader.appendChild(node);
 
-      //
-      let headText = document.getElementsByClassName("subHead");
+      //Get all subHeader divs by class name
+      let getSubheaderDivHead = document.getElementsByClassName("subHeader");
 
-      for(i=0;i< headText.length; i++) {
-        headText[i].appendChild(subHeader);
+      //Loop through all subHeader divs and attach subheader names
+      for(let i=0;i< getSubheaderDivHead.length; i++) {
+        getSubheaderDivHead[i].appendChild(subHeader);
       }
 
-      key['menuItem'].forEach(thing =>{
-        let name = thing.head;
-        let price = " \u20AC" + thing.price;
-        let name_price = name + price;
+      //Create Menu Wrapper
+      let menuWrapper = document.createElement("div");
+      menuWrapper.setAttribute("class", "menuWrapper");
 
+      let getSubheaderDivWrap = document.getElementById("menuDiv");
+      getSubheaderDivWrap.appendChild(menuWrapper);
 
-        let itemDiv = document.createElement("div");
-        itemDiv.setAttribute("class", "item");
+      menuSection['menuItem'].forEach(menuItem =>{
 
-        let athing = document.getElementById("mainDiv");
-        athing.appendChild(itemDiv);
+        let name = menuItem.head;
+        let vegetarian = menuItem.vegetarian;
+        let price = "\u00A3" + menuItem.price;
+        let subHeading = menuItem.subhead;
+        let description = menuItem.description;
 
-
-        //Create P tag
-        var para = document.createElement("p");
-        var info = document.createTextNode(name_price);
-        para.appendChild(info);
-
-        var desc = document.createElement("p");
-        var desc_txt = document.createTextNode(thing.description);
-        desc.appendChild(desc_txt);
-
-
-        var element = document.getElementsByClassName("item");
-        //itemDiv.appendChild(para);
-
-        for(i=0;i< element.length; i++) {
-          element[i].appendChild(para);
-          element[i].appendChild(desc);
+        if(vegetarian){
+          console.log(name);
         }
 
+        //Create menu item div
+        let itemDiv = document.createElement("div");
+        itemDiv.setAttribute("class", "menuItem");
+
+        //Get menu wrapper
+        let getMenuWrapper = document.getElementsByClassName("menuWrapper");
+
+        //Attach menu item div to menu wrapper
+        for(let i=0;i< getMenuWrapper.length; i++) {
+          getMenuWrapper[i].appendChild(itemDiv);
+        }
+
+        //Create menu item wrapper
+        let menuItemWrapper = document.createElement("div");
+        menuItemWrapper.setAttribute("class", "menuItemWrapper");
+
+        //Get menu item divs
+        let getMenuItem = document.getElementsByClassName("menuItem");
+
+        for(let i=0;i< getMenuItem.length; i++) {
+          getMenuItem[i].appendChild(menuItemWrapper);
+        }
+
+        //Create menu item divs
+        let menuItemHeaderDiv = document.createElement("div");
+        menuItemHeaderDiv.setAttribute("class", "menuItemHeader");
+
+        let menuItemPriceDiv = document.createElement("div");
+        menuItemPriceDiv.setAttribute("class", "menuItemPrice");
+
+        let menuItemDescriptionDiv = document.createElement("div");
+        menuItemDescriptionDiv.setAttribute("class", "menuItemDescription");
+
+        //Get menu item divs
+        let getMenuItemWrapper = document.getElementsByClassName("menuItemWrapper");
+
+        for(let i=0;i< getMenuItemWrapper.length; i++) {
+          getMenuItemWrapper[i].appendChild(menuItemHeaderDiv);
+          getMenuItemWrapper[i].appendChild(menuItemPriceDiv);
+          getMenuItemWrapper[i].appendChild(menuItemDescriptionDiv);
+        }
+
+        let menuItemHeaderH4 = document.createElement('h4');
+        let menuItemHeaderText = document.createTextNode(name);
+        menuItemHeaderH4.appendChild(menuItemHeaderText);
+
+        let getMenuItemHeaderH4 = document.getElementsByClassName('menuItemHeader');
+
+        for(let i=0;i< getMenuItemHeaderH4.length; i++) {
+          getMenuItemHeaderH4[i].appendChild(menuItemHeaderH4);
+        }
+
+        let menuItemPrice = document.createElement('p');
+        let menuItemPriceText = document.createTextNode(price);
+        menuItemPrice.appendChild(menuItemPriceText);
+
+        let getMenuItemPrice = document.getElementsByClassName('menuItemPrice');
+
+        for(let i=0;i< getMenuItemPrice.length; i++) {
+          getMenuItemPrice[i].appendChild(menuItemPrice);
+        }
+
+        let menuItemDescription = document.createElement('p');
+        let menuItemDescriptionText = document.createTextNode(description);
+        menuItemDescription.appendChild(menuItemDescriptionText);
+
+        let getMenuItemDescription = document.getElementsByClassName('menuItemDescription');
+
+        for(let i=0;i< getMenuItemDescription.length; i++) {
+          getMenuItemDescription[i].appendChild(menuItemDescription);
+        }
       });
-
-      console.log(key.name);
     });
-
-
   }
-
 });
